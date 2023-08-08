99ec8e4e3c3fc0baba855bc4001bfd25f75796a3
facudacosta1 <facurivero29@gmail.com>
2023-08-08 14:33:06 -0300

    subida de solucion click

--- eventos.js
+++ eventos.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("div-boton").onclick = function(){
-        mostrarSaludo();
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("div-boton").onclick = function(event) {
+        if (event.target.tagName !=="BUTTON"){ //Si la etiqueta es diferente a BUTTON no se llama a la function mostrarSaludoDiv();
+            mostrarSaludoDiv();
+        }
+    };
+
+    function mostrarSaludoDiv(){
+        alert("Hola! Soy el div");
     }
 });
 
-function mostrarSaludo(){
-    alert("Hola! Soy el div")
-};
+//Notas: en el event.target.tagName se debe colocar el nombre de la etiqueta en mayusculas siempre
